50f04bffa2878e28417c9a07583dce38c3b16325
Michael Weyman <weymanm1@nku.edu>
2015-11-02 16:27:43 -0500

    adding branches
    hjgdsig

---
+++ MasterBranch/master.java
@@ -0,0 +1,10 @@
+package MasterBranch;
+
+public class master {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ testing/jUnitTesting.java
@@ -0,0 +1,10 @@
+package testing;
+
+public class jUnitTesting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
